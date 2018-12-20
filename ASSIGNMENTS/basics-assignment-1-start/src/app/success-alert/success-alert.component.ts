1adef5737a076e2b419325cc6a467ab48124f313
Jonathan <archie88a+gitkraken@gmail.com>
2018-12-20 23:38:34 +1100

    Completed Assignment 1, created 2 Components and styled them.

---
+++ ASSIGNMENTS/basics-assignment-1-start/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  template: `
+    <div class="alert alert-success">
+      <strong>Success!</strong> Indicates a successful or positive action.
+    </div>`
+})
+export class SuccessAlertComponent {
+}
