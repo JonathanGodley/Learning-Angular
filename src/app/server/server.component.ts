003b0b8fa1a8dfe08a414d95312794331b40f6c5
Jonathan <archie88a+gitkraken@gmail.com>
2018-12-20 23:49:59 +1100

    String interpolation implemented

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+  // can also specify type, serverId: number = 10;
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
