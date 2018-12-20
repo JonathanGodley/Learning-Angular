036d8216aaab1c704416aa0f3500a94824f96708
Jonathan <archie88a+gitkraken@gmail.com>
2018-12-21 00:58:33 +1100

    Assignment 2 Complete

--- ASSIGNMENTS/basics-assignment-2-start/src/app/app.component.ts
+++ ASSIGNMENTS/basics-assignment-2-start/src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username = '';
+  enableButton = false;
+
+  onUpdateUsername(event: any) {
+    this.enableButton = (<HTMLInputElement>event.target).value !== '';
+  }
+
+  onClearUsername() {
+    this.username = '';
+    this.enableButton = false;
+  }
 }
