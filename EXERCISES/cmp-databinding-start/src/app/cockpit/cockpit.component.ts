f3e352b8dbad914ffa37dd2565a3dc5bfde91d62
Jonathan <archie88a+gitkraken@gmail.com>
2018-12-21 22:41:46 +1100

    Local References implemented in cockpit

--- EXERCISES/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ EXERCISES/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -8,23 +8,22 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
   newServerContent = '';
 
   constructor() { }
 
   ngOnInit() {
   }
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
+      serverName: nameInput.value,
       serverContent: this.newServerContent});
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     {
       this.blueprintCreated.emit({
-        serverName: this.newServerName,
+        serverName: nameInput.value,
         serverContent: this.newServerContent});
     }
    }
