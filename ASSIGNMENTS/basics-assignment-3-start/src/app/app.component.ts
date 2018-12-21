25ea3f53bbc97fa47ed8ba0b57c493b4f209cc08
Jonathan <archie88a+gitkraken@gmail.com>
2018-12-21 14:20:05 +1100

    Finished A3, added a note to A2

--- ASSIGNMENTS/basics-assignment-3-start/src/app/app.component.ts
+++ ASSIGNMENTS/basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  infoVisible = false;
+  clicks = [];
+
+  onToggleInfo() {
+    this.infoVisible = !this.infoVisible;
+    this.clicks.push(new Date().toString());
+  }
 }
