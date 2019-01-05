9d75e233a49b975de2e949ad9079f58a93628665
Jonathan <archie88a+gitkraken@gmail.com>
2019-01-05 11:43:10 +1100

    Fetch Route Parameters Reactively

--- EXERCISES/routing-start/src/app/users/user/user.component.ts
+++ EXERCISES/routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy{
   user: {id: number, name: string};
+  paramsSubscription: Subscription
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,6 +18,18 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    this.paramsSubscription = this.route.params.subscribe(
+      (params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    // shouldn't need to do this for this specific observable,
+    // but if we add our own observables we need to unsubscribe on our own.
+    this.paramsSubscription.unsubscribe();
   }
 
 }
